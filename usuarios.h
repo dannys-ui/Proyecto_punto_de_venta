968389b8288240bf396b8a510d154effb1089b78
dannys-ui <danny.espinosa.20b@gmail.com>
2026-07-10 09:05:21 -0500

    Definir archivo usuarios.h con struct Usuario (uid, clave, nombre, rol, puntos) y enum Rol

---
+++ usuarios.h
@@ -0,0 +1,19 @@
+#ifndef USUARIOS_H //para evitar que se incluya varias veces el mismo archivo
+#define USUARIOS_H //si no existe entonces se define y la crea
+typedef enum { //se define un tipo de dato enumerado llamado Rol
+    ADMINISTRADOR = 1, //rol administrador = 1, los otros valores se asignan automáticamente a partir de 1
+    CAJERO, //rol cajero con el valor 2 automáticamente asignado
+    BODEGUERO, // igual que cajera se asigno automáticamente el valor 3
+    COMPRADOR, // 4
+} Rol; //se llama rol a estos datos y se lo debe llamar con ese nombre mas adelante
+typedef struct { //se define un tipo de dato estructurado llamado Usuario
+    char uid_nfc[20]; //arreglo de tamaño 20 para guardar la clave de las tarjetas nfc
+    char clave_teclado[20]; //arreglo auxiliar para ingresar la clave del usuario por teclado
+    char nombre_usuario[50]; //arreglo para el nombre del usuario
+    Rol rol; //cada usuario tendra un campo llamado rol y ese campo solo puede tomar un valor de rol.
+    int puntos; //se define un campo de tipo int para almacenar los puntos del usuario
+} Usuario; //se llama Usuario a estos datos y se lo debe llamar con ese nombre mas adelante
+
+void crearArchivoSemilla(); //archivo "seed" o semilla que se genera por primera con datos de prueba.
+void leerUsuariosRegistrados(); //funcion que lee los usuarios registrados en el archivo de usuarios
+#endif // USUARIOS_H
