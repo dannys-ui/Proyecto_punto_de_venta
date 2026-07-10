b264e905efd0b9eaebc036782e94242d76ab0c3e
dannys-ui <danny.espinosa.20b@gmail.com>
2026-07-09 22:23:18 -0500

    primer commit: estructura inicial

---
+++ punto_de_venta.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+//* structura de los usuarios
+typedef struct
+{
+    char clave[20];
+    char rol[20]
+} Usuarios; //! Nombre de la estructura para llamar mas adelante
+
+
